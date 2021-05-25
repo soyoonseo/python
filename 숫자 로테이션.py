404ce30bdf66585fa15f31ca6ce2562afeeeed83
soyoonseo <42333011+soyoonseo@users.noreply.github.com>
2021-05-25 21:13:55 +0900

    Add files via upload

---
+++ 숫자 로테이션.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr = arr*2
+
+for i in range(0,n):
+          for k in range(i,n+i):
+                    print(arr[k], end=" ")
+          print()
+          
+          
+          
+
+          
+          
+                    
+                    
+                    
+
