37cf5b19d1c2ff92db2ce17410e0ae834ce345b7
soyoonseo <42333011+soyoonseo@users.noreply.github.com>
2021-06-21 10:40:40 +0900

    Update 유클리드.py

--- 유클리드.py
+++ 유클리드.py
@@ -1,3 +1,4 @@
+#최대공약수
 def f(n,m):
     if(n < m):
         n , m = m, n
@@ -8,3 +9,22 @@ def f(n,m):
           
 n, m = map(int, input().split())
 f(n,m)
+
+
+
+#최대공약수, 최소공배수
+a, b=map(int, input().split())
+
+def gcd(a,b):
+    if(a<b):
+        a,b=b,a
+    while(b!= 0):
+        a = a%b
+        a,b = b,a
+    return a
+        
+def lcm(a,b):
+    return a*b//gcd(a,b)
+
+print(gcd(a,b))
+print(lcm(a,b))
